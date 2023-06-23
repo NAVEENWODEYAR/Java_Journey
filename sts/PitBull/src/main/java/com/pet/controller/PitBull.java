0cab4018a95cd637a1c23b685073b340c252fac9
Naveen <naveenkrishnawodeyar@gmail.com>
2023-06-23 13:57:20 +0530

    ga

--- sts/PitBull/src/main/java/com/pet/controller/PitBull.java
+++ sts/PitBull/src/main/java/com/pet/controller/PitBull.java
@@ -1,10 +1,13 @@
 package com.pet.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
+@Controller
 public class PitBull 
 {
 	@RequestMapping("/p")
@@ -13,7 +16,7 @@ public class PitBull
 		System.out.println("Displays age..,");
 		return "28";
 	}
-	@RequestMapping("/p1")
+	@GetMapping("/p1")
 	public ModelAndView getIndex()
 	{
 		System.out.println("index page is waiting.,");
---
+++ sts/mvcExTwo/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,3 @@
+<h2 style="color: aquamarine; font-size: 40rem;">
+    Welcome to the Spring Boot home..,
+</h2>
