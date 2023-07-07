e1ea9f2a5a20ed4d947b7ef71407c660e99a9462
Naveen <naveenkrishnawodeyar@gmail.com>
2023-07-08 00:34:19 +0530

    gn

--- sts/dataJpa/src/main/java/com/dataJpa/controller/BookController.java
+++ sts/dataJpa/src/main/java/com/dataJpa/controller/BookController.java
@@ -3,6 +3,7 @@ package com.dataJpa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.dataJpa.madal.Book;
 import com.dataJpa.service.BookService;
 
 @RestController
+@CrossOrigin()
 public class BookController
 {
 	@Autowired
