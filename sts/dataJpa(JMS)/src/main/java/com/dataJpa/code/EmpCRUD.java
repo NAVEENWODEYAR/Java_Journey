6dfbdaab88cfaead94c9225472f25ef925617e76
Naveen <naveenkrishnawodeyar@gmail.com>
2023-07-24 12:23:45 +0530

    ga

---
+++ sts/dataJpa(JMS)/src/main/java/com/dataJpa/code/EmpCRUD.java
@@ -0,0 +1,14 @@
+package com.dataJpa.code;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EmpCRUD {
+
+	public static void main(String[] args)
+	{
+		// insert the dummy values 
+		List<Employee> emList = Arrays.asList(new Employee());
+	}
+
+}
