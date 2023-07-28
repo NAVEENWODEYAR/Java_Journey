82f5088f8cf44b09ba55b0033df3dfed00982a8e
Naveen <naveenkrishnawodeyar@gmail.com>
2023-07-28 20:48:34 +0530

    gn

---
+++ sts/dataJpa(JMS)/src/main/java/com/data/j8coding/fi/FindAnyEx.java
@@ -0,0 +1,14 @@
+package com.data.j8coding.fi;
+
+import java.util.*;
+
+public class FindAnyEx {
+
+	public static void main(String[] args) 
+	{
+		// anyMatch()-  Returns true if any one element of a stream matches with given predicate.,
+		List<Integer> list = Arrays.asList(2,3,4,5,6,7,8,9);
+				System.out.println(list);
+	}
+
+}
