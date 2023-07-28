237144680beb2d611771ccb49a994faf920445de
Naveen <naveenkrishnawodeyar@gmail.com>
2023-07-28 16:21:39 +0530

    ge

---
+++ sts/dataJpa(JMS)/src/main/java/com/data/j8coding/StrmEx.java
@@ -0,0 +1,22 @@
+package com.data.j8coding;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StrmEx 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Stream is sequence of elements from an sourcewhich supports the data processing.,");
+		List<String> list = Arrays.asList("Annie","Rooney","Mooney","Nancy","Bennie");
+			for(String st: list)
+			{
+				if((st.length() >= 5))
+				{
+					System.out.println(st.toUpperCase());
+				}				
+			}
+		
+	}
+
+}
