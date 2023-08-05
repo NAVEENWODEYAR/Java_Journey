5e56fad0f181868b257e25e2b9ac9959ff736d06
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-05 13:49:33 +0530

    ga

--- sts/dataJpa(JMS)/src/main/java/com/data/codefi/map/MegerMap.java
+++ sts/dataJpa(JMS)/src/main/java/com/data/codefi/map/MegerMap.java
@@ -1,27 +1,31 @@
 package com.data.codefi.map;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class MegerMap {
 
 	public static void main(String[] args) 
 	{
-		HashMap<String, Integer> marksList = new HashMap<>();
+		Map<String, Integer> marksList = new HashMap<>();
 								marksList.put("Science",87);
 								marksList.put("Social",96);
 								marksList.put("Maths",81);
 								marksList.put("GK",99);
 								marksList.put("Lan",91);
 		
-		HashMap<String, Integer> marksList2 = new HashMap<>();
-								marksList2.put("Science",87);
+		Map<String, Integer> marksList2 = new HashMap<>();
+								marksList2.put("Science",89);
 								marksList2.put("Social",96);
-								marksList2.put("Maths",81);
-								marksList2.put("GK",99);
-								marksList2.put("Lan",91);
+								marksList2.put("Maths",89);
+								marksList2.put("GK",91);
+								marksList2.put("Computer",81);
 								
 								marksList.putAll(marksList2);
+								for(Map.Entry<String, Integer>mp: marksList.entrySet())
+								{
+									System.out.println(mp.getKey()+":"+mp.getValue());
+								}
+								
 								
 
 	}
