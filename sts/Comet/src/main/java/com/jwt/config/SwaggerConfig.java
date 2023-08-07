ca7ef72b5b81c6037f4bdeaec1e6ab878704e341
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-07 15:13:16 +0530

    swagerConfig added

--- sts/Comet/src/main/java/com/jwt/config/SwaggerConfig.java
+++ sts/Comet/src/main/java/com/jwt/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.jwt.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -15,7 +17,7 @@ import static springfox.documentation.builders.PathSelectors.regex;
 public class SwaggerConfig
 {
 
-	// 
+	/**
 	@Bean
 	public Docket postApi()
 	{
@@ -32,11 +34,19 @@ public class SwaggerConfig
 	{
 		return new ApiInfoBuilder().title("Employee CRUD")
 									.description("Demo documentation generated using the SwaggerAPI for the REST API")
-									.termsOfServiceUrl("Samle")
+									.termsOfServiceUrl("Sample")
 									.license("MIT")
 									.licenseUrl("MIT.lab")
 									.version("2.0")
 									.build();
-									
-	}
+									*/
+		@Bean
+		public Docket api()
+		{
+			return new Docket(DocumentationType.SWAGGER_2)
+					.select()
+					.apis(RequestHandlerSelectors.basePackage("com.jwt.controller"))
+					.paths(PathSelectors.any())
+					.build();
+		}
 }
