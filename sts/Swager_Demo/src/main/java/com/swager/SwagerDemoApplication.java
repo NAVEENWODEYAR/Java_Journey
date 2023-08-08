0cc4f5321ddfe5aeb931f76d70a93bc31ca5de6d
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-08 18:58:04 +0530

    swager API added

--- sts/Swager_Demo/src/main/java/com/swager/SwagerDemoApplication.java
+++ sts/Swager_Demo/src/main/java/com/swager/SwagerDemoApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SwagerDemoApplication {
+public class SwagerDemoApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(SwagerDemoApplication.class, args);
 	}
 
