45d24fcbe5a78b741926c89cb795e20c05a6aa1f
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-08 20:56:41 +0530

    openapi

--- sts/Swager_Demo/src/main/java/com/swager/controller/Swagger_Controller.java
+++ sts/Swager_Demo/src/main/java/com/swager/controller/Swagger_Controller.java
@@ -7,6 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 @RequestMapping("/s")
 public class Swagger_Controller 
@@ -18,6 +24,16 @@ public class Swagger_Controller
 	{
 		return "Welcome to Swagger API.,";
 	}
+	
+	
+	// adding the description about the API.,
+	@Operation(summary = "Get Names,", description = "Returns the names list from the configured database.," ,tags = "Get")
+	@ApiResponses(value = {
+							@ApiResponse(responseCode = "200K",description = "Found the List",
+										content = {@Content(mediaType = "application/json",
+										schema = @Schema(implementation = List.class))}),
+							@ApiResponse(responseCode = "404",description = "Not Found",content = @Content)
+	})
 	
 	@GetMapping("/getList")
 	public List getList()
