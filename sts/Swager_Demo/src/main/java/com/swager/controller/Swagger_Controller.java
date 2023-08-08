5e5ac5fb338b9e8d3755ddfe263680df7288f19b
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-08 20:03:21 +0530

    openapi ui

--- sts/Swager_Demo/src/main/java/com/swager/controller/Swagger_Controller.java
+++ sts/Swager_Demo/src/main/java/com/swager/controller/Swagger_Controller.java
@@ -1,11 +1,14 @@
 package com.swager.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping("/s")
 public class Swagger_Controller 
 {
 	// dummy API for testing
@@ -14,5 +17,12 @@ public class Swagger_Controller
 	public String getSwagerHome()
 	{
 		return "Welcome to Swagger API.,";
+	}
+	
+	@GetMapping("/getList")
+	public List getList()
+	{
+		List<String> list = Arrays.asList("Ammu","Bhas");
+		return list;
 	}
 }
