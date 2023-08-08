5db339b1d230f6e03b46e0b075cd938b24ac6ecf
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-08 10:45:31 +0530

    h2

--- sts/Comet/src/main/java/com/jwt/config/SwaggerConfig.java
+++ sts/Comet/src/main/java/com/jwt/config/SwaggerConfig.java
@@ -11,12 +11,11 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import static springfox.documentation.builders.PathSelectors.regex;
 
-@Configuration
 @EnableSwagger2
 public class SwaggerConfig
 {
 
-	/**
+	
 	@Bean
 	public Docket postApi()
 	{
@@ -38,14 +37,15 @@ public class SwaggerConfig
 									.licenseUrl("MIT.lab")
 									.version("2.0")
 									.build();
-									*/
-		@Bean
-		public Docket api()
-		{
-			return new Docket(DocumentationType.SWAGGER_2)
-					.select()
-					.apis(RequestHandlerSelectors.basePackage("com.jwt.controller"))
-					.paths(PathSelectors.any())
-					.build();
-		}
+	}
+									
+//		@Bean
+//		public Docket api()
+//		{
+//			return new Docket(DocumentationType.SWAGGER_2)
+//					.select()
+//					.apis(RequestHandlerSelectors.basePackage("com.jwt.controller"))
+//					.paths(PathSelectors.any())
+//					.build();
+//		}
 }
