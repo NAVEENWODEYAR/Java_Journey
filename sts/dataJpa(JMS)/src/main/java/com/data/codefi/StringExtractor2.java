4bc425cc68e0b6c746635394ebd99d59d3614f65
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-10 00:13:21 +0530

    log

---
+++ sts/dataJpa(JMS)/src/main/java/com/data/codefi/StringExtractor2.java
@@ -0,0 +1,37 @@
+// Program to extract the numbers,characters and symbols from an string.,
+package com.data.codefi;
+
+public class StringExtractor2 {
+
+	public static void main(String[] args) 
+	{
+		String st= "Naveen@123#$Kumar!456K";
+		StringBuilder characters = new StringBuilder();
+		StringBuilder numbers = new StringBuilder();
+		StringBuilder symbols = new StringBuilder();
+		
+			
+			for(int i=0; i<st.length(); i++)
+			{
+				if(Character.isAlphabetic(st.charAt(i)))
+				{
+					characters.append(st.charAt(i));
+				}
+				else if(Character.isDigit(st.charAt(i)))
+				{
+					numbers.append(st.charAt(i));
+				}
+				else
+					symbols.append(st.charAt(i));
+					
+			}
+			
+			
+			System.out.println("Characters,"+characters);
+			System.out.println("Numbers,"+numbers);
+			System.out.println("Symbols,"+symbols);
+
+			
+	}
+
+}
