39ce202efead4d38f4925930a5d0283c290975f5
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-12 21:03:42 +0530

    bean validation

---
+++ sts/Bateleur/src/test/java/com/aquila/BateleurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BateleurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
