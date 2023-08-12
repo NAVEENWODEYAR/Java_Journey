6585f9f3ab3fc5819cf0141d6696923743b402ef
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-12 23:38:42 +0530

    DTO class

--- sts/Bateleur/src/main/java/com/aquila/BateleurApplication.java
+++ sts/Bateleur/src/main/java/com/aquila/BateleurApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BateleurApplication {
+public class BateleurApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(BateleurApplication.class, args);
 	}
 
