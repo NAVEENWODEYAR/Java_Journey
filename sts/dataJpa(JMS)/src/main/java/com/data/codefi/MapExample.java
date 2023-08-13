18d614317db3c7ad80ce69de9f8d808267a493b6
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-13 21:48:17 +0530

    sun

---
+++ sts/dataJpa(JMS)/src/main/java/com/data/codefi/MapExample.java
@@ -0,0 +1,20 @@
+package com.data.codefi;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapExample {
+
+	public static void main(String[] args) 
+	{
+		Map<Integer, String> mp = new HashMap<>();
+							mp.put(1, "One");
+							mp.put(2, "Two");
+							
+			mp.forEach((k,v)->System.out.println(k+":"+v));
+			mp.entrySet().stream().filter(k->k.getKey()%2==0)
+			.forEach(System.out::println);
+			
+	}
+
+}
