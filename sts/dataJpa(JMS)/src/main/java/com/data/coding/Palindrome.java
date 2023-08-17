36a440cf9ec83beed75d97a13cb933680f2674c3
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-17 13:43:02 +0530

    strFns

--- sts/dataJpa(JMS)/src/main/java/com/data/coding/Palindrome.java
+++ sts/dataJpa(JMS)/src/main/java/com/data/coding/Palindrome.java
@@ -2,9 +2,26 @@ package com.data.coding;
 
 public class Palindrome {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void palindrome(String st)
+	{
+		StringBuilder reverse = new StringBuilder();
+					reverse.append(st);
+					reverse.reverse();
+					
+			if(st.contentEquals(reverse))
+			{
+				System.out.println("Palindrome");
+			}
+			else
+			{
+				System.out.println("Not an palindrome");
+			}
+		
+	}
+	public static void main(String[] args)
+	{
+		String str = args[0];
+		palindrome(str);
 	}
 
 }
---
+++ sts/dataJpa(JMS)/src/main/java/com/data/coding/RemoveWhiteSpaceFromString.java
@@ -0,0 +1,10 @@
+package com.data.coding;
+
+public class RemoveWhiteSpaceFromString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
