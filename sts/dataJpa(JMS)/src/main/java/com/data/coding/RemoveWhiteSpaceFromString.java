32ca89186ff6e057689c4690ac10159c9e1d9e07
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-17 13:53:43 +0530

    lh

--- sts/dataJpa(JMS)/src/main/java/com/data/coding/RemoveWhiteSpaceFromString.java
+++ sts/dataJpa(JMS)/src/main/java/com/data/coding/RemoveWhiteSpaceFromString.java
@@ -2,9 +2,23 @@ package com.data.coding;
 
 public class RemoveWhiteSpaceFromString {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static String removeWhiteSpace(String str)
+	{
+		StringBuilder sb = new StringBuilder();
+			
+		for(char c: str.toCharArray())
+		{
+			if(!Character.isWhitespace(c))
+				sb.append(c);		
+		}
+		return sb.toString();
+		
+			
+	}
+	public static void main(String[] args) 
+	{
+		System.out.println(removeWhiteSpace("Java Programming"));
+	
 	}
 
 }
