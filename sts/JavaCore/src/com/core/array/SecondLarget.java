8b9eabb4b12490be7e24112f344c7e09e0c47cf3
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-18 21:47:07 +0530

    gn

---
+++ sts/JavaCore/src/com/core/array/SecondLarget.java
@@ -0,0 +1,40 @@
+// Write a Java Program to find the second-highest number in an array.
+package com.core.array;
+
+import java.util.Arrays;
+
+public class SecondLarget
+{
+	public static void secLargest(int[] arr)
+	{
+		int largest = 0;
+		int secLargest = 0;
+		
+			for(int i=0; i<arr.length; i++)
+			{
+				if(arr[i] > largest)
+				{
+					secLargest = largest;
+					largest = arr[i];
+				}
+				else if(arr[i] > secLargest)
+				{
+					secLargest = arr[i];
+				}
+			}
+			System.out.println("\nSecond Largset,"+secLargest);
+			System.out.println("\nLargest,"+largest);
+
+				
+	}
+	 static public void  main(String[] args) 
+	{
+		int ar[] = {1,4,643,8,5,4,4,6,87};
+		String arr[] = {"Ammu","Boss","Ammie","Denly","Gowri","Gani"};
+		System.out.println(Arrays.toString(ar));
+		System.out.println(Arrays.asList(arr));
+
+		secLargest(ar);
+	}
+
+}
