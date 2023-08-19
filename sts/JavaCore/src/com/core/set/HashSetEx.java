1ee5b6cb31336eda6e8b98ab87a6785ed0dbd9c1
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-19 13:53:26 +0530

    TreeSet

---
+++ sts/JavaCore/src/com/core/set/HashSetEx.java
@@ -0,0 +1,26 @@
+package com.core.set;
+
+import java.util.*;
+
+public class HashSetEx
+{
+
+	public static void main(String[] args)
+	{
+		Set set = new HashSet<>();
+			set.add("One");
+			set.add("Three");
+			set.add("Two");
+			set.add("Four");
+			set.add("null");
+			set.add(null);
+			Iterator itr = set.iterator();
+			while(itr.hasNext())
+			{
+				System.out.println(itr.next());
+			}
+			
+			set.parallelStream().sorted().forEach(System.out::println);
+					
+	}
+}
