ea3fdca65e35c7fd811daf8252c12e8c02271b16
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-19 14:14:19 +0530

    ga

--- sts/JavaCore/src/com/core/set/TreeMapEx.java
+++ sts/JavaCore/src/com/core/set/TreeMapEx.java
@@ -7,21 +7,23 @@ public class TreeMapEx {
 
 	public static void main(String[] args) 
 	{
+		System.out.println("TreeMap");
 		Map<Integer, String> mp = new TreeMap<>();
 							mp.put(1, "One");
 							mp.put(2, "Two");
 							mp.put(10, "Ten");
-							mp.put(5, "Five");
+//							mp.put(null, "Five");		//Exception 
 							mp.put(7, "Seven");
 							
 			mp.entrySet().stream().forEach(System.out::println);
-	
+			
+			System.out.println("\nHashMap");
 			Map<Integer, String> hmp = new HashMap<>();
-			hmp.put(1, "One");
+			hmp.put(1, null);
 			hmp.put(2, "Two");
 			hmp.put(10, "Ten");
 			hmp.put(5, "Five");
-			hmp.put(7, "Seven");
+			hmp.put(null, "Seven");
 			
 			hmp.entrySet().stream().forEach(System.out::println);
 	}
