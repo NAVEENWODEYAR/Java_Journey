83c63c06a7bfffb96231f7dadf0308f5e5cc833c
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-19 13:24:21 +0530

    tree

---
+++ sts/JavaCore/src/com/core/set/TreeSetEx.java
@@ -0,0 +1,22 @@
+package com.core.set;
+
+import java.util.*;
+
+public class TreeSetEx
+{
+
+	public static void main(String[] args)
+	{
+		TreeSet<Integer> set = new TreeSet<>();
+					set.add(1);
+					set.add(5);
+//					set.add(null);
+			System.out.println("Treeset doedn't allows null values");	
+			Iterator itr = set.iterator();
+			while(itr.hasNext())
+			{
+				System.out.println(itr.next());
+			}
+					
+	}
+}
