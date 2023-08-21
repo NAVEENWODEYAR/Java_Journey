50896dc7525c97fd699641345d2ed8976af0ca44
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-21 14:02:15 +0530

    swagger ui added

--- sts/Tawny/src/main/java/com/aquila/TawnyApplication.java
+++ sts/Tawny/src/main/java/com/aquila/TawnyApplication.java
@@ -3,7 +3,24 @@ package com.aquila;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+						title = "LapTop",
+						version = "2.0.0",
+						description = "API Documentation,",
+						termsOfService = "Test the API",
+						contact = @Contact(
+											name = "Boss",
+											email = "boss@boss.org"),
+						license = @License(
+											name = "Boss",
+											url = "@BOSS@")))
 public class TawnyApplication 
 {
 
--- sts/Tawny/src/main/java/com/aquila/controller/TeacherController.java
+++ sts/Tawny/src/main/java/com/aquila/controller/TeacherController.java
@@ -1,5 +1,17 @@
 package com.aquila.controller;
 
-public class TeacherController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/t")
+public class TeacherController 
+{
+	
+	@GetMapping("/getMsg")
+	public String getMsg()
+	{
+		return "Sky is not the limit!";
+	}
 }
