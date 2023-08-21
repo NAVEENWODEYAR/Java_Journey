058dd7c4f3c9bd48703ce0d69db7aeef05e32577
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-21 13:20:39 +0530

    tawny started

---
+++ sts/Tawny/src/main/java/com/aquila/TawnyApplication.java
@@ -0,0 +1,16 @@
+package com.aquila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TawnyApplication 
+{
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(TawnyApplication.class, args);
+		System.out.println("Flying towards target");
+	}
+
+}
