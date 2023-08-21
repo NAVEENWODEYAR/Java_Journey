d3dc98bf09e6436e6f328cda09e3c134d405d7f8
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-21 22:47:04 +0530

    map()

---
+++ sts/JavaCore/src/com/core/j8/MapEx.java
@@ -0,0 +1,22 @@
+//Java 8 program to multiply 3 to all elements in the list and print the list?
+
+package com.core.j8;
+
+import java.util.Arrays;
+
+public class MapEx 
+{
+	
+	public static void multNumbers(int arr[])
+	{
+		Arrays.stream(arr)
+				.map(n -> n*3)
+					.forEach(System.out::println);
+	}
+	public static void main(String[] args) 
+	{
+		multNumbers(new int[] {1,2,3,4,5,6,7,8,9});
+		System.out.println("**********");
+	}
+
+}
