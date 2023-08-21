f5d390bc2288376140ce6f7e582b546f3dda0c82
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-21 12:32:57 +0530

    ga

---
+++ sts/JavaCore/src/com/core/map/CharFrequency.java
@@ -0,0 +1,32 @@
+// Program to find the character frequency in the string
+package com.core.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CharFrequency 
+{
+	public static void charFrequency(String str)
+	{
+		Map<Character, Integer> mp = new HashMap<>();
+		
+			for(char c: str.toCharArray())
+			{
+				if(mp.containsKey(c))
+				{
+					mp.put(c, mp.get(c)+1);
+				}
+				else 
+				{
+					mp.put(c, 1);
+				}
+			}
+			System.out.println(mp.toString());
+	}
+	
+	public static void main(String[] args)
+	{
+		charFrequency("String");
+	}
+
+}
