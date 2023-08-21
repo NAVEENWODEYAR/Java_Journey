ccf3294f75655a854d59cbc951d16ece1c69eb18
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-21 16:04:14 +0530

    setters and getters generated

--- sts/Tawny/src/main/java/com/aquila/modal/Teacher.java
+++ sts/Tawny/src/main/java/com/aquila/modal/Teacher.java
@@ -1,7 +1,18 @@
 package com.aquila.modal;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Teacher 
 {
+	@Id
 	int tId;
 	String tName;
 	String tAddress;
