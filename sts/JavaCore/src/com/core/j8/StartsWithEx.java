53c22990c60debda256beb1d673db97543656d2a
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-21 23:24:38 +0530

    startsWith()

---
+++ sts/JavaCore/src/com/core/j8/StartsWithEx.java
@@ -0,0 +1,23 @@
+//Given a list of integers, find out all the numbers starting with 1 using Stream functions?
+
+package com.core.j8;
+
+import java.util.*;
+
+public class StartsWithEx 
+{
+	public static void startsWith1(List<Integer> list)
+	{
+		list.parallelStream()
+			.map(n -> n+"")
+			.filter(n -> n.toString().startsWith("1"))
+			.forEach(System.out::println);
+	}
+
+	public static void main(String[] args) 
+	{
+		List<Integer> list = Arrays.asList(1,2,45,12,24,15,16,27,19,18);
+		startsWith1(list);
+	}
+
+}
