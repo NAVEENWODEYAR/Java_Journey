59beb5319a32fe7ac9221604bb27ec422026946f
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-21 22:16:43 +0530

    random

---
+++ sts/JavaCore/src/com/core/j8/RandomEx.java
@@ -0,0 +1,13 @@
+// Java 8 Program to Print ten random numbers using forEach?
+
+package com.core.j8;
+
+public class RandomEx 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println(Math.random());
+	}
+
+}
