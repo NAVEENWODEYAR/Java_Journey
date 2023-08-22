7b7f8ce8c2b240b8fd09bb12884efac88ee7c293
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-22 16:28:21 +0530

    validations added

--- sts/Tawny/src/main/java/com/aquila/dto/TeacherDTO.java
+++ sts/Tawny/src/main/java/com/aquila/dto/TeacherDTO.java
@@ -1,5 +1,8 @@
 package com.aquila.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +12,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class TeacherDTO
 {
+	@NotEmpty(message = "Name can't be empty")
 	String tName;
+	
+	@NotEmpty(message = "Please provide your address")
 	String tAddress;
+	
+	@NotNull
+	@Min(message = "Should be greater than the specified value",value = 46789)
 	double tSalary;
+	
+	@NotEmpty(message = "Enter proper mail id")
 	String tmail;
+	
+	@NotEmpty(message = "Subject should be unique")
 	String tSubject;
 }
