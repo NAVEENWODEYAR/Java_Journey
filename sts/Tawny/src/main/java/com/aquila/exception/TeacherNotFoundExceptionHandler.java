ef63f602c4a4bd0c802a0cc2520e2f2ac3201b42
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-22 16:36:12 +0530

    MethodArgumentNotValidException
    Bad Request

--- sts/Tawny/src/main/java/com/aquila/exception/TeacherNotFoundExceptionHandler.java
+++ sts/Tawny/src/main/java/com/aquila/exception/TeacherNotFoundExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.aquila.exception;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,5 +21,16 @@ public class TeacherNotFoundExceptionHandler
 	{
 		ResponseDTO responseDTO = new ResponseDTO("Sorry exception occured while seraching for the reacord which is not present!",exception);
 		return new ResponseEntity<>(responseDTO, HttpStatus.NOT_FOUND);
+	}
+	
+	@ExceptionHandler
+	public ResponseEntity<ResponseDTO> illegalArgumentExceptionHandler(MethodArgumentNotValidException exception)
+	{
+		List<ObjectError> errList = exception.getBindingResult().getAllErrors();
+		List<String> errMsg = errList.stream()
+									.map(e -> e.getDefaultMessage())
+									.collect(Collectors.toList());
+		ResponseDTO responseDTO = new ResponseDTO("Teacher not Found in the database..,",errMsg);
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
 	}
 }
