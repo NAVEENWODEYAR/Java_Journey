577c19ab995e4a7e8707798dd38d1a62b0e1426e
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-22 15:51:47 +0530

    Service class added

--- sts/Tawny/src/main/java/com/aquila/service/TeacherService.java
+++ sts/Tawny/src/main/java/com/aquila/service/TeacherService.java
@@ -1,5 +1,43 @@
 package com.aquila.service;
 
-public class TeacherService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aquila.dto.TeacherDTO;
+import com.aquila.modal.Teacher;
+import com.aquila.repo.TeacherRepo;
+
+@Service
+public class TeacherService {
+	// DI
+	@Autowired
+	private TeacherRepo teacherRepo;
+
+	// CRUD operations., insert to the table
+	public Teacher insertTeacher(TeacherDTO teacherDTO)
+	{
+		Teacher t = new Teacher(teacherDTO);
+				return teacherRepo.save(t);
+	}
+	
+	// get the data
+	public List<Teacher> getTeachersList()
+	{
+		List<Teacher> list = teacherRepo.findAll();
+					return list;
+	}
+	
+	// get the record
+	public Teacher getTeacher(Integer tId)
+	{
+		return teacherRepo.findById(tId).get();
+	}
+	
+	// update the record
+	public Teacher updateTeacher(TeacherDTO teacherDTO, Integer tId)
+	{
+		Teacher t = teacherRepo.findById(tId).get();
+	}
 }
