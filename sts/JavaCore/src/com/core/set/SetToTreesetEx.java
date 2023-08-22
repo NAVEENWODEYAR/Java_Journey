34c1d5c503be260954499ce38c986dee5a71a191
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-22 11:59:45 +0530

    TreeSet

--- sts/JavaCore/src/com/core/set/SetToTreesetEx.java
+++ sts/JavaCore/src/com/core/set/SetToTreesetEx.java
@@ -10,13 +10,15 @@ public class SetToTreesetEx
 	public static void main(String[] args)
 	{
 		List<Integer> list = List.of(1,3,4,5,787,9,1,2,3,4,5,6,4,3,2,1);
-					System.out.println(list);
+					System.out.println("Original list provided "+list);
 					System.out.println(list.subList(2, 8));
+					System.out.println("*************");
 					
 		Set<Integer> set = list.stream().collect(Collectors.toSet());
-					System.out.println(set);
-		Set<Integer> hashSet = new HashSet<>(set);
-					System.out.println(hashSet);
+					System.out.println("Set obtained from the list "+set);
+					System.out.println("***************");
+		Set<Integer> treeSet = new TreeSet<>(set);
+					System.out.println("TreeSet obtained from the set "+treeSet);
 	
 	}
 
