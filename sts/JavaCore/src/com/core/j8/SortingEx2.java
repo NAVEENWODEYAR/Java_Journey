7fad02bc1b889285cd6d1d02af15467c71395544
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-22 11:44:47 +0530

    gm

---
+++ sts/JavaCore/src/com/core/j8/SortingEx2.java
@@ -0,0 +1,28 @@
+// Given a list of integers, sort all the values present in it using Stream functions?
+package com.core.j8;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SortingEx2 
+{
+	static void sortList(List<Integer> list)
+	{
+		list.stream()
+			.collect(Collectors.toSet())
+			.forEach(System.out::println);
+	}
+	public static void main(String[] args)
+	{
+		System.out.println("List provided,");
+		List<Integer> myList = Arrays.asList(10,15,8,49,25,98,98,32,15,1,0,1,2,3,4);
+
+        myList.stream()
+              .sorted()
+              .forEach(System.out::println);
+        
+        System.out.println("Set obtained,");
+        sortList(myList);
+	}
+
+}
