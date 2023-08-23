bac5b4a9ca183ab1f8c524dc2234f38f44b000f6
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-23 13:52:24 +0530

    []+

--- sts/JavaCore/src/com/core/regex/RegEx.java
+++ sts/JavaCore/src/com/core/regex/RegEx.java
@@ -7,13 +7,13 @@ public class RegEx
 {
 	public static void main(String[] args)
 	{
-		Pattern p = Pattern.compile("an");
-		Matcher m = p.matcher(".n");
+		Pattern p = Pattern.compile(".m");
+		Matcher m = p.matcher("ammie");
 		boolean b = m.matches();
 		System.out.println(b);
 		
 		System.out.println(Pattern.matches("[a-z A-Z]+", "Java"));
-		System.out.println(Pattern.matches("[\\w]", "Java123"));
+		System.out.println(Pattern.matches("[\\w]+", "Java123"));
 
 	}
 }
