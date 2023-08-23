d1f1bef70c8023628d9e94d1a8144df659fcf72b
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-23 15:58:14 +0530

    Threads

--- sts/JavaCore/src/com/core/thread/ThreadEx.java
+++ sts/JavaCore/src/com/core/thread/ThreadEx.java
@@ -2,12 +2,21 @@ package com.core.thread;
 
 public class ThreadEx extends Thread
 {
+	@Override
 	public void run()
 	{
 		Thread t1 = new Thread();
 				for(int i=0; i<10; i++)
 				{
-					t1.start();
+					System.out.println(i);
+					
+					try 
+					{
+						Thread.sleep(2000);
+					} catch (InterruptedException e)
+					{
+						e.printStackTrace();
+					}
 				}
 	}
 	public static void main(String[] args) 
