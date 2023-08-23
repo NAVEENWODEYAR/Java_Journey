7437976639a06b59b19af357cd5e9825ee232a4c
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-23 13:41:45 +0530

    pattern

--- sts/JavaCore/src/com/core/regex/RegEx.java
+++ sts/JavaCore/src/com/core/regex/RegEx.java
@@ -8,7 +8,12 @@ public class RegEx
 	public static void main(String[] args)
 	{
 		Pattern p = Pattern.compile("an");
-		Matcher m = p.matcher(".abc");
-		System.out.println(Pattern.matches("[a-z]+", "Java"));
+		Matcher m = p.matcher(".n");
+		boolean b = m.matches();
+		System.out.println(b);
+		
+		System.out.println(Pattern.matches("[a-z A-Z]+", "Java"));
+		System.out.println(Pattern.matches("[\\w]", "Java123"));
+
 	}
 }
