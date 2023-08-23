408dd86dbebb31579b39c2a289f8ed684ec781d4
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-23 16:05:28 +0530

    join

--- sts/JavaCore/src/com/core/thread/ThreadEx.java
+++ sts/JavaCore/src/com/core/thread/ThreadEx.java
@@ -5,24 +5,26 @@ public class ThreadEx extends Thread
 	@Override
 	public void run()
 	{
-		Thread t1 = new Thread();
 				for(int i=0; i<10; i++)
 				{
 					System.out.println(i);
 					
 					try 
 					{
-						Thread.sleep(2000);
+						Thread.sleep(500);
 					} catch (InterruptedException e)
 					{
 						e.printStackTrace();
 					}
 				}
 	}
-	public static void main(String[] args) 
+	public static void main(String[] args) throws InterruptedException 
 	{
-		ThreadEx ob = new ThreadEx();
-				ob.run();
+		ThreadEx t1 = new ThreadEx();
+		ThreadEx t2 = new ThreadEx();
+				t2.start();
+				t2.join();
+				t1.start();
 	}
 
 }
