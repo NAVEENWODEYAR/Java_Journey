47d76b304cf9c8624607e13ef5b923e40b0ed0d2
Naveen <naveenkrishnawodeyar@gmail.com>
2023-08-29 17:21:51 +0530

    TrianglePattern of numbers

--- sts/dataJpa(JMS)/src/main/java/com/data/code/pattern/TrianglePattern1.java
+++ sts/dataJpa(JMS)/src/main/java/com/data/code/pattern/TrianglePattern1.java
@@ -1,6 +1,6 @@
 package com.data.code.pattern;
 
-public class TrianglePattern 
+public class TrianglePattern1 
 {
 
 	private static void trianglePattern()
@@ -9,14 +9,14 @@ public class TrianglePattern
 		{
 			for(int j=0; j<=i; j++)
 			{
-				System.out.print(j+" ");
+				System.out.print("*"+" ");
 			}
 			System.out.println( );
 		}
 	}
 	public static void main(String[] args)
 	{
-		System.out.println("Triangle Pattern(Numbers) using for loop,");
+		System.out.println("Triangle Pattern using for loop,");
 		trianglePattern();
 	}
 
