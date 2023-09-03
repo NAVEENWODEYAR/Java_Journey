1dd794a0e4c5b10a8c84be50e5c4586294e5d732
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-03 22:46:53 +0530

    gn

--- sts/JavaCore/src/com/core/j8/EvenOddNumbers.java
+++ sts/JavaCore/src/com/core/j8/EvenOddNumbers.java
@@ -17,7 +17,7 @@ public class EvenOddNumbers
 	{
 		List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
 		
-		System.out.println("Even numbers present in the list ");
+		System.out.println("Even numbers present in the given list ");
 		oddEvenExractor(list);
 	}
 
