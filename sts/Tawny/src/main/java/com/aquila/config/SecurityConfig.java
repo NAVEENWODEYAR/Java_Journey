57cd97a4f44370ea912e5cd137a865376aa51224
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-05 18:32:03 +0530

    @Bean

--- sts/Tawny/src/main/java/com/aquila/config/SecurityConfig.java
+++ sts/Tawny/src/main/java/com/aquila/config/SecurityConfig.java
@@ -1,11 +1,22 @@
 package com.aquila.config;
 
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig 
 {
-	
+	@Bean
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception 
+	{
+		http.authorizeHttpRequests().anyRequest().authenticated();
+		http.formLogin();
+		http.httpBasic();
+		return http.build();
+	}
 }
