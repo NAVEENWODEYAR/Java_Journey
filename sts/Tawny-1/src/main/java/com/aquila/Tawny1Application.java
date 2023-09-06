64970499c429448b2129f5842b3db0841d18088e
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-06 13:48:22 +0530

    version

---
+++ sts/Tawny-1/src/main/java/com/aquila/Tawny1Application.java
@@ -0,0 +1,13 @@
+package com.aquila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tawny1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tawny1Application.class, args);
+	}
+
+}
---
+++ sts/Tawny-1/src/test/java/com/aquila/Tawny1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tawny1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
