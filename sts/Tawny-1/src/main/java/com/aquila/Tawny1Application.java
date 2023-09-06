c841f3f47f4aca38e8a43b44582fe6fb6a1c7f9c
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-06 14:20:12 +0530

    ga

--- sts/Tawny-1/src/main/java/com/aquila/Tawny1Application.java
+++ sts/Tawny-1/src/main/java/com/aquila/Tawny1Application.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Tawny1Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(Tawny1Application.class, args);
 	}
 
