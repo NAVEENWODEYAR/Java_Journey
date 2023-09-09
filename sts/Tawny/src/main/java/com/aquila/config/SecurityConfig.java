5196eeae2110bbbe90c3e872c5e3f5c8ba431644
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-10 02:05:48 +0530

    GM

--- sts/Tawny/src/main/java/com/aquila/config/SecurityConfig.java
+++ sts/Tawny/src/main/java/com/aquila/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.aquila.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
