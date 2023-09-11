6645df5dce08a4ab8ddd8a557428c42b5c19476d
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-11 15:46:46 +0530

    Singleton class will have only one instance through out your application

--- sts/JavaCore/src/com/core/cls/SingleTonEx.java
+++ sts/JavaCore/src/com/core/cls/SingleTonEx.java
@@ -6,6 +6,7 @@ public class SingleTonEx
 	public static void main(String[] args)
 	{
 		System.out.println("SingleTon Class,");
+		System.out.println("Singleton class will have only one instance through out your application");
 	}
 
 }
