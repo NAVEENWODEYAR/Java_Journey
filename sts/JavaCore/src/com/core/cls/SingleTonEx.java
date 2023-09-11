c7cb6dde0aebedf708c66c48d384712bdfe7e583
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-11 16:03:51 +0530

    error

--- sts/JavaCore/src/com/core/cls/SingleTonEx.java
+++ sts/JavaCore/src/com/core/cls/SingleTonEx.java
@@ -2,11 +2,14 @@ package com.core.cls;
 
 public class SingleTonEx
 {
-
+	
 	public static void main(String[] args)
+	{
+		
+	}
+	// instance block
 	{
 		System.out.println("SingleTon Class,");
 		System.out.println("Singleton class will have only one instance through out your application");
 	}
-
 }
