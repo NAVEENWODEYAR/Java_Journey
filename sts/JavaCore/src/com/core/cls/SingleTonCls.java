bd4738b767f359d825efbabc385780c0ca36106d
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-11 16:00:37 +0530

    private String s;

--- sts/JavaCore/src/com/core/cls/SingleTonCls.java
+++ sts/JavaCore/src/com/core/cls/SingleTonCls.java
@@ -3,11 +3,12 @@ package com.core.cls;
 public class SingleTonCls
 {
 	private static SingleTonCls instance=null;
+	private String s;
 	
 	// private constructor
 	private SingleTonCls()
 	{
-		
+		s="Private constructor";
 	}
 	
 	public SingleTonCls getInstance()
