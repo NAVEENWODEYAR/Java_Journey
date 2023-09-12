37d31e2d586d2a0682c48e0efc40bf505a466166
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-12 11:28:22 +0530

    Shallow copy

--- sts/JavaCore/src/com/core/cls/ColoneOfSingleTon.java
+++ sts/JavaCore/src/com/core/cls/ColoneOfSingleTon.java
@@ -3,13 +3,15 @@ package com.core.cls;
 public class ColoneOfSingleTon 
 {
 
-	public static void main(String[] args) 
+	public static void main(String[] args) throws CloneNotSupportedException 
 	{
 		SingleTonCls obj = SingleTonCls.getInstance();
-					System.out.println(obj.hashCode());
+					System.out.println("Original object hashcode "+obj.hashCode());
 					
 		// Cloning 
-				
+		SingleTonCls clonedInstance = (SingleTonCls) obj.clone();	
+		System.out.println("Cloned object hashcode "+clonedInstance.hashCode());
+		System.out.println(obj==clonedInstance);
 	}
 
 }
