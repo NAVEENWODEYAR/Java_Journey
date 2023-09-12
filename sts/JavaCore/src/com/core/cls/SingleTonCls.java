1c8cdd141b44fea0325d72cfeadcf6825720ffad
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-12 11:16:34 +0530

    Class<?> singleTon = Class.forName("com.core.cls.SingleTonEx");

--- sts/JavaCore/src/com/core/cls/SingleTonCls.java
+++ sts/JavaCore/src/com/core/cls/SingleTonCls.java
@@ -33,16 +33,12 @@ public class SingleTonCls
 					System.out.println("Hashcode of the object c "+c.hashCode());
 					System.out.println(c==b);
 					System.out.println(c.equals(b));
-					
-					
+								
 		// reflection to break singleton and make prototype
 		Class<?> singleTon = Class.forName("com.core.cls.SingleTonEx");
 		Constructor<SingleTonEx> constructor = (Constructor<SingleTonEx>) singleTon.getDeclaredConstructor(null);
 		constructor.setAccessible(true);
 		SingleTonEx instance = constructor.newInstance();
 		System.out.println("Hashcode of the reflection object "+instance.hashCode());
-		
-	
-	}
-	
+	}	
 }
