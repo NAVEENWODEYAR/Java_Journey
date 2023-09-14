49f3561f8cd485f3c389550c7282211058474a66
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-14 22:56:20 +0530

    Serializable.class.isInterface()

--- sts/JavaCore/src/com/core/revised/SingleTonClass.java
+++ sts/JavaCore/src/com/core/revised/SingleTonClass.java
@@ -1,5 +1,7 @@
 package com.core.revised;
 
+import java.io.Serializable;
+
 public class SingleTonClass
 {
 	private static SingleTonClass obj=null;
@@ -23,6 +25,11 @@ public class SingleTonClass
 		SingleTonClass s1 = SingleTonClass.getObject();
 		SingleTonClass s2 = SingleTonClass.getObject();
 		SingleTonClass s3 = new SingleTonClass();
+		
+		// check weather it is interface or not
+		System.out.println(s3.getClass().getInterfaces());
+		if(Serializable.class.isInterface())
+			System.out.println(true);
 		
 		System.out.println(s1.hashCode()+"--"+s1.equals(s2)+"--"+s2.hashCode());
 		System.out.println(s2.hashCode()+"--"+s2.equals(s3)+"--"+s3.hashCode());
