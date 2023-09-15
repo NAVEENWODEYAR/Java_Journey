82cf3f20b2449f7c80bef0f968d10b9bd0f8bd00
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-15 20:31:43 +0530

    @Service @Component

--- sts/Tawny/src/main/java/com/aquila/service/TeacherService.java
+++ sts/Tawny/src/main/java/com/aquila/service/TeacherService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import com.aquila.dto.TeacherDTO;
@@ -13,6 +14,7 @@ import com.aquila.repo.TeacherRepo;
 import com.aquila.util.TeacherMails;
 
 @Service
+@Component
 public class TeacherService {
 	// DI
 	@Autowired
