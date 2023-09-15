ab71cf202f82e7d3d6b29b68af212003f9ff632d
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-15 19:11:24 +0530

    - @org.springframework.beans.factory.annotation.Autowired(required=true)

--- sts/Tawny/src/main/java/com/aquila/config/SecurityConfig.java
+++ sts/Tawny/src/main/java/com/aquila/config/SecurityConfig.java
@@ -1,20 +1,20 @@
-package com.aquila.config;
-
-import org.springframework.context.annotation.*;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-public class SecurityConfig 
-{
-//	@Bean
-//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-// 		http
-// 			.authorizeHttpRequests((authorizeHttpRequests) ->
-// 				authorizeHttpRequests
-// 					.requestMatchers("/**").hasRole("USER")
-// 			)
-// 			.formLogin();
-// 		return http.build();
-//	}
-}
+//package com.aquila.config;
+//
+//import org.springframework.context.annotation.*;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//public class SecurityConfig 
+//{
+////	@Bean
+////	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//// 		http
+//// 			.authorizeHttpRequests((authorizeHttpRequests) ->
+//// 				authorizeHttpRequests
+//// 					.requestMatchers("/**").hasRole("USER")
+//// 			)
+//// 			.formLogin();
+//// 		return http.build();
+////	}
+//}
