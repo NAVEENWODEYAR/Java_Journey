6c36788ab4517d6c5d4353770dad48565f04dfd2
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-16 19:58:19 +0530

    Arrays.sort(a,Collections.reverseOrder())

--- sts/JavaCore/src/com/core/interviewquesions/SortArray.java
+++ sts/JavaCore/src/com/core/interviewquesions/SortArray.java
@@ -1,12 +1,17 @@
 package com.core.interviewquesions;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class SortArray 
 {
 	static void sortArray(int[] arr)
 	{
-		int[] array = Arrays.stream(arr).sorted().toArray();
+		int[] array = Arrays.stream(arr).distinct().sorted().toArray();
+		String arryList = Arrays.toString(arr);
+		System.out.println(arryList);
+
 		for(int n: array)
 		{
 			System.out.println(n);
@@ -14,7 +19,11 @@ public class SortArray
 	}
 	public static void main(String[] args) 
 	{
-		sortArray(new int[]{1,9,3,2,6,7,5,4});
+		sortArray(new int[]{1,9,4,1,3,2,6,7,5,4});
+		
+		 Integer  a[] = {1,9,4,1,3,2,6,7,5,4};
+		Arrays.sort(a,Collections.reverseOrder());
+		System.out.println(Arrays.toString(a));
 	}
 	static
 	{
