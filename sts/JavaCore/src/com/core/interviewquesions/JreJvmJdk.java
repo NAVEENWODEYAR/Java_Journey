e61309b6fb6ead7f0dffd9f1c86b4becebe231e8
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-16 13:59:57 +0530

    jdk,jvm,jre in java

---
+++ sts/JavaCore/src/com/core/interviewquesions/JreJvmJdk.java
@@ -0,0 +1,20 @@
+package com.core.interviewquesions;
+
+public class JreJvmJdk
+{
+	static
+	{
+		System.out.println("JDK, The Java Development Kit (JDK) is a software development \r\n"
+				+ "environment used for developing Java applications. ");
+	}
+	static
+	{
+		System.out.println("JRE,It consists of the Java Virtual Machine\r\n"
+				+ "    core classes, and supporting files");
+	}
+	static
+	{
+		System.out.println("JVM, is an abstract virtual machine that provides a runtime environment for running Java bytecode.");
+	}
+
+}
