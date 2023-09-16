eed845dcfea4051430112c5f1ecd8cdb77f7aeeb
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-16 20:35:04 +0530

    rev += st.charAt(i);

--- sts/JavaCore/src/com/core/interviewquesions/PalindromeEx.java
+++ sts/JavaCore/src/com/core/interviewquesions/PalindromeEx.java
@@ -14,6 +14,25 @@ public class PalindromeEx
 	public static void main(String[] args) 
 	{
 		palindromeTest("RaceCar");
+		palindromeTest("deified");
+		palindromeTest("level");
+		palindromeTest("rotor");
+		palindromeTest("civic");
+		palindromeTest("malayalam");
+	}
+	
+	static
+	{
+		String st = "racecar";
+		String rev = "";
+			
+			for(int i=st.length()-1; i>=0; i--)
+			{
+				rev += st.charAt(i);
+			}
+			System.out.println("\n Static Block");
+			System.out.println("Given string("+st+") is an palindrome "+st.equals(rev));
+			System.out.println("***********");
 	}
 
 }
