dd5d517dbe10668e7f529a10281bb27d2e9af11c
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-18 02:03:19 +0530

    HikariPool-1 - Shutdown completed.

--- sts/Bateleur/src/test/java/com/aquila/BateleurApplicationTests.java
+++ sts/Bateleur/src/test/java/com/aquila/BateleurApplicationTests.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BateleurApplicationTests {
+class BateleurApplicationTests 
+{
 
 	@Test
-	void contextLoads() {
+	void contextLoads() 
+	{
+		
 	}
 
 }
