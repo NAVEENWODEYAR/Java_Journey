13f924d4467910a2beeb9a95d5ef7a4ad06394ff
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-17 14:22:50 +0530

    java.lang.ArrayIndexOutOfBoundsException

---
+++ sts/JavaCore/src/com/core/string/WordFreq2.java
@@ -0,0 +1,30 @@
+//Write a Java Program to count the number of words in a string using HashMap.
+package com.core.string;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WordFreq2 
+{
+	public static void wordFrequency(String str)
+	{
+		String [] st = str.split(" ");
+		Map<String, Integer> hashMap = new HashMap<>();
+		
+			for(int i=0; i<str.length(); i++)
+			{
+				if(hashMap.containsKey(st[i]))
+				{
+					hashMap.put(st[i], hashMap.get(st[i]+1));
+				}
+				else
+					hashMap.put(st[i], 1);
+			}
+			System.out.println(hashMap);
+	}
+	public static void main(String[] args) 
+	{
+		wordFrequency("Java Program Hello World, Hello Java Family, Java loves you");
+	}
+
+}
