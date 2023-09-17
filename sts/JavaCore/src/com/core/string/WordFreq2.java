c06d34bfd0a09e5c560f99f5592688ee308f611b
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-17 14:37:20 +0530

    exception

--- sts/JavaCore/src/com/core/string/WordFreq2.java
+++ sts/JavaCore/src/com/core/string/WordFreq2.java
@@ -8,32 +8,26 @@ public class WordFreq2
 {
 	public static void wordFrequency(String str)
 	{
-		String [] st = str.split(" ");
+		String st[] = str.split(" ");
 		Map<String, Integer> hashMap = new HashMap<>();
-		
-			for(int i=0; i<st.length-1; i++)
+			
+			for(int i=0; i<= st.length-1; i++)
 			{
 				if(hashMap.containsKey(st[i]))
 				{
 					hashMap.put(st[i], hashMap.get(st[i]+1));
 				}
 				else
+				{
 					hashMap.put(st[i], 1);
+				}
 			}
 			
-			for(int i=0; i<=st.length-1; i++)
-			{
-				if(mp.containsKey(st[i]))
-				{
-					mp.put(st[i], mp.get(st[i])+1);					
-				}
-				else
-					mp.put(st[i], 1);
 			System.out.println(hashMap);
 	}
 	public static void main(String[] args) 
 	{
-		wordFrequency("Java Program Hello World, Hello Java Family,Java loves you");
+		wordFrequency("Java Program Hello World Hello Java Family Java loves you");
 	}
 
 }
