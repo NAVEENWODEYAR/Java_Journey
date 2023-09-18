1e6a377c2613bcb1c72a23de4435c976c9e5c6c6
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-18 12:48:32 +0530

    Logger log = LoggerFactory.getLogger(BookStoreController.class)

--- sts/BookStoreUsingMangoe/src/main/java/com/bookstore/controller/BookStoreController.java
+++ sts/BookStoreUsingMangoe/src/main/java/com/bookstore/controller/BookStoreController.java
@@ -2,6 +2,8 @@ package com.bookstore.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
@@ -26,6 +28,11 @@ public class BookStoreController
 {
 	@Autowired
 	private BookStoreService bsService;
+	
+	// adding the logger
+	
+	 Logger log = LoggerFactory.getLogger(BookStoreController.class);
+
 
 		// 1. generating the API.,
 			@PostMapping("/insertBookStoreData")
