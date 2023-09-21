210379dc830facd12db2dde92c531ad012aaf072
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-21 12:23:33 +0530

    SecurityFilterChain

--- sts/Tawny/src/main/java/com/aquila/config/SecurityConfig.java
+++ sts/Tawny/src/main/java/com/aquila/config/SecurityConfig.java
@@ -1,25 +1,25 @@
-package com.aquila.config;
-
-import org.springframework.context.annotation.*;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-public class SecurityConfig 
-{
-	@Bean
-    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception
-    {
-        http
-                .csrf().disable()
-                .authorizeHttpRequests()
-                .requestMatchers("/car/test").permitAll()
-                .requestMatchers("/car").authenticated()
-
-                .and().formLogin()
-                .and().httpBasic();
-        
-        return http.build();
-
-    }
-}
+//package com.aquila.config;
+//
+//import org.springframework.context.annotation.*;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//public class SecurityConfig 
+//{
+//	@Bean
+//    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception
+//    {
+//        http
+//                .csrf().disable()
+//                .authorizeHttpRequests()
+//                .requestMatchers("/car/test").permitAll()
+//                .requestMatchers("/car").authenticated()
+//
+//                .and().formLogin()
+//                .and().httpBasic();
+//        
+//        return http.build();
+//
+//    }
+//}
