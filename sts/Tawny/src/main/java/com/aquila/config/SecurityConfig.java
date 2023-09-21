29031e162a5d7ff6916049b3e9fedf368701ec5c
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-21 09:21:02 +0530

    imports errors

--- sts/Tawny/src/main/java/com/aquila/config/SecurityConfig.java
+++ sts/Tawny/src/main/java/com/aquila/config/SecurityConfig.java
@@ -1,25 +1,11 @@
 package com.aquila.config;
 
 import org.springframework.context.annotation.*;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig 
 {
-////	@Bean
-////	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//// 		http
-//// 			.authorizeHttpRequests((authorizeHttpRequests) ->
-//// 				authorizeHttpRequests
-//// 					.requestMatchers("/**").hasRole("USER")
-//// 			)
-//// 			.formLogin();
-//// 		return http.build();
-////	}
-//}
-
-@Bean
+	@Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception
     {
         http
