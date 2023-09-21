7c67db7cc47ec8277ea4bf5f3be43ac5bcecce4e
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-21 09:18:29 +0530

    SecurityFilterChain

--- sts/Tawny/src/main/java/com/aquila/config/SecurityConfig.java
+++ sts/Tawny/src/main/java/com/aquila/config/SecurityConfig.java
@@ -1,12 +1,12 @@
-//package com.aquila.config;
-//
-//import org.springframework.context.annotation.*;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//public class SecurityConfig 
-//{
+package com.aquila.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig 
+{
 ////	@Bean
 ////	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 //// 		http
@@ -18,3 +18,18 @@
 //// 		return http.build();
 ////	}
 //}
+
+@Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception
+    {
+        http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/car/test").permitAll()
+                .requestMatchers("/car").authenticated()
+
+                .and().formLogin()
+                .and().httpBasic();
+
+        return http.build();
+    }
