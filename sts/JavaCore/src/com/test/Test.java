2da3ba7c4f0483b96d2383c38b07f68acf6fc7f3
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-22 20:12:04 +0530

    Test

---
+++ sts/JavaCore/src/com/test/Test.java
@@ -0,0 +1,28 @@
+package com.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test
+{
+	public static void charFrequency(String st)
+	{
+		String str[] = st.split("");
+		Map<String, Integer> mp = new HashMap<>();
+		
+		for(int i=0; i<str.length-1; i++)
+		{
+			if(mp.containsKey(str[i]))
+				mp.put(str[i], mp.get(str[i])+1);
+			else
+				mp.put(str[i], 1);
+		}
+		System.out.println(mp);
+	}
+	public static void main(String[] args) 
+	{
+		String st = "Naveen Kumar K";
+		charFrequency(st);
+	}
+
+}
