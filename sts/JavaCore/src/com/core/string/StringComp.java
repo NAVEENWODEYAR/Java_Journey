185e8dbaf9d2a5c7d3bfcde69f2f9019740f0cd5
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-30 13:54:40 +0530

    if(!(obj instanceof Student))

--- sts/JavaCore/src/com/core/string/StringComp.java
+++ sts/JavaCore/src/com/core/string/StringComp.java
@@ -47,7 +47,12 @@ public class StringComp {
 	@Override
 	public boolean equals(Object obj) 
 	{
-		return super.equals(obj);
+		if(obj == this)
+			return true;
+		if(!(obj instanceof Student))
+			return false;
+		Student s = (Student)obj;
+		return String.c
 	}
 
 }
