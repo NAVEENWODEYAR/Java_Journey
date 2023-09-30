a6db16f5638e8e47b322c5dece78ca0287b4fd9e
Naveen <naveenkrishnawodeyar@gmail.com>
2023-09-30 13:36:58 +0530

    @Override 	public boolean equals(Object obj)

--- sts/JavaCore/src/com/core/string/StringComp.java
+++ sts/JavaCore/src/com/core/string/StringComp.java
@@ -21,20 +21,33 @@ public class StringComp {
 		// compare Student objects,
 		Student st1 = new Student("Amma");
 		Student st2 = new Student("Amma");
+		objComparision(st1, st2);
 		
 	}
 	static void strCompare(String s1,String s2)
 	{
 		System.out.println("\nString comparision,\n");
+		
 		if(s1 == s2)
 			System.out.println("True");
 		else
 			System.out.println("False");
 	}
 	
-	static void objComparision(Student s1, Student s2s)
+	static void objComparision(Student s1, Student s2)
 	{
 		System.out.println("\n Object Comparision,\n");
+		
+		if(s1.equals(s2))
+			System.out.println("Both the objects are equal");
+		else
+			System.out.println("Both the objects are not equal");
+		
+	}
+	@Override
+	public boolean equals(Object obj) 
+	{
+		return super.equals(obj);
 	}
 
 }
