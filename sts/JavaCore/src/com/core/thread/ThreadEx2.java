1031a388b99af117fd90ee13f1f07fa50232f8b4
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-10 22:15:41 +0530

    public class ThreadEx2 implements Runnable

--- sts/JavaCore/src/com/core/thread/ThreadEx2.java
+++ sts/JavaCore/src/com/core/thread/ThreadEx2.java
@@ -12,19 +12,19 @@ public class ThreadEx2 implements Runnable
 					try 
 					{
 						Thread.sleep(500);
-					} catch (InterruptedException e)
+					} 
+					catch (InterruptedException e)
 					{
 						e.printStackTrace();
 					}
 				}
 	}
+	
 	public static void main(String[] args) throws InterruptedException 
 	{
 		ThreadEx2 t1 = new ThreadEx2();
 		ThreadEx2 t2 = new ThreadEx2();
 				t1.run();
-				t2.run();
-			
+				t2.run();	
 	}
-
 }
