4cefb2b6ba57e7afaed9613abef88dc47388add3
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-11 09:46:08 +0530

    GM

--- sts/JavaCore/src/com/core/interviewquesions/SingleTonClsImpl.java
+++ sts/JavaCore/src/com/core/interviewquesions/SingleTonClsImpl.java
@@ -22,7 +22,7 @@ public class SingleTonClsImpl
 			e.printStackTrace();
 		}
 		
-		// reflection to break the singleton class design pattern,
+		// reflection to break the singleton,
 		Class<?> reflection = Class.forName("com.core.interviewquesions");
 		Constructor<SingleTonCls> constructor = (Constructor<SingleTonCls>) reflection.getDeclaredConstructor(null);
 									constructor.setAccessible(true);
