c22df5d68f47ca8acb066441201568a88546280f
Naveen Kumar K <108418552+NAVEENWODEYAR@users.noreply.github.com>
2025-02-15 23:24:59 +0530

    Update Stak.java

--- IDE/Boss/src/cF1/Stak.java
+++ IDE/Boss/src/cF1/Stak.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class Stak {
 
 	public static void main(String[] args) {
-		// The stack is the subclass of Vector. It implements the last-in-first-out data structure
+		// Stack is the subclass of Vector. It implements the last-in-first-out(LIFO) data structure
 		Stack st = new Stack();
 				st.push("Julie");
 				st.push("Serena");
@@ -12,15 +12,14 @@ public class Stak {
 				st.add("Melina");
 				st.pop();
 				st.remove("Melina");
-				st.add(3, "Melina");
-				st.set(3, "Rory");
+				st.add(3,"Melina");
+				st.set(3,"Rory");
 				System.out.println(st);
 				
 			// Iterating using Iterator
 			System.out.println("Elements of the Stack ");
 			Iterator it = st.iterator();
-				while(it.hasNext())
-				{
+				while(it.hasNext()){
 					System.out.println(it.next());
 				}
 					
