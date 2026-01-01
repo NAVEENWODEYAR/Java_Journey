be00c92a1fc87799b3cc93f4b080ba97ce02e372
Naveen Kumar K <108418552+NAVEENWODEYAR@users.noreply.github.com>
2026-01-01 22:32:35 +0530

    Update ConsoleEx.java

--- Programs/ConsoleEx.java
+++ Programs/ConsoleEx.java
@@ -4,7 +4,7 @@ public class ConsoleEx {
 	public static void main(String[] args) {
 		// Console class is be used to get input from console. It provides methods to read texts and passwords
 		Console c = System.console();
-		System.out.println("Enter your name");
+		System.out.println("Enter your name: ");
 		String str = c.readLine("Name", "ID");
 		System.out.println(c);
 
